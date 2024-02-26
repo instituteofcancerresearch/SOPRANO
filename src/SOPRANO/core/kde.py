84e288b35e36b751995e3e5559010e915e568376
KareemMarzouk <bkmarzouk@outlook.com>
2024-02-26 16:28:25 +0000

    add classes for estimator derived quantities and figure components

--- src/SOPRANO/core/kde.py
+++ src/SOPRANO/core/kde.py
@@ -1,4 +1,6 @@
+import os
 import warnings
+from dataclasses import dataclass
 from typing import Callable
 
 import numpy as np
@@ -194,3 +196,189 @@ def _estimate_std_deviation(
     sample_values: np.ndarray,
 ):
     return sample_values.std()
+
+
+def _data_available(null_hypothesis_samples_split: pd.DataFrame):
+    return null_hypothesis_samples_split.shape[0] != 0
+
+
+@dataclass
+class EstimatorResults:
+    estimator: None | Callable
+    integration_bounds: None | tuple
+    pvalues: None | tuple
+    std: None | float
+
+    @classmethod
+    def null(cls):
+        return cls(None, None, None, None)
+
+
+_ABS_TOL = 1e-8
+_REL_TOL = 1e-8
+_STEP_PCT = 1e-2
+_MAX_ITER = 1000
+
+_ABS_TOL_ENV_VAR = "SOPRANO_KDE_ABS_TOL"
+_REL_TOL_ENV_VAR = "SOPRANO_KDE_REL_TOL"
+_STEP_PCT_ENV_VAR = "SOPRANO_KDE_STEP_PCT"
+_MAX_ITER_ENV_VAR = "SOPRANO_KDE_MAX_ITER"
+
+
+class _Data:
+    def __init__(
+        self,
+        null_hypothesis_samples: pd.DataFrame,
+        data: pd.DataFrame,
+        column_key: str,
+    ):
+        self.null_hypothesis_samples = null_hypothesis_samples
+        self.data_value = data[column_key].mean()
+        self.column_key = column_key
+        self.is_available = _data_available(null_hypothesis_samples)
+
+        if self.is_available:
+            self.estimates = self._apply_estimator()
+        else:
+            self.estimates = EstimatorResults.null()
+
+    def _apply_estimator(self) -> EstimatorResults:
+        estimator = _probability_estimator(
+            self.null_hypothesis_samples, self.column_key
+        )
+
+        samples = self.null_hypothesis_samples[self.column_key]
+
+        integration_bounds = _determine_integration_bounds(
+            estimator=estimator,
+            sample_values_min=samples.min(),
+            sample_values_max=samples.max(),
+            absolute_tol=float(os.getenv(_ABS_TOL_ENV_VAR, _ABS_TOL)),
+            relative_tol=float(os.getenv(_REL_TOL_ENV_VAR, _REL_TOL)),
+            step_percent=float(os.getenv(_STEP_PCT_ENV_VAR, _STEP_PCT)),
+            max_iterations=int(os.getenv(_MAX_ITER_ENV_VAR, _MAX_ITER)),
+        )
+
+        pvalues = _estimate_pvalue(
+            estimator, self.data_value, *integration_bounds
+        )
+
+        std = _estimate_std_deviation(samples)
+
+        return EstimatorResults(
+            estimator=estimator,
+            integration_bounds=integration_bounds,
+            pvalues=pvalues,
+            std=std,
+        )
+
+
+class _HistogramData(_Data):
+    def __init__(
+        self,
+        null_hypothesis_samples: pd.DataFrame,
+        data_results: pd.DataFrame,
+        column_key: str,
+        sample_label: str,
+        sample_color: str,
+        sample_alpha: float,
+        sample_hatch: str | None,
+        sample_hist_type: str,
+        data_label: str,
+        data_color: str,
+        data_line_style: str,
+    ):
+        super().__init__(
+            null_hypothesis_samples=null_hypothesis_samples,
+            data=data_results,
+            column_key=column_key,
+        )
+
+        self.sample_histogram_kwargs = {
+            "bins": "auto",
+            "density": True,
+            "label": sample_label,
+            "facecolor": sample_color,
+            "edgecolor": sample_color,
+            "alpha": sample_alpha,
+            "hatch": sample_hatch,
+            "histtype": sample_hist_type,
+        }
+
+        self.sample_kde_kwargs = {
+            "color": sample_color,
+        }
+
+        self.data_histogram_kwargs = {
+            "label": data_label,
+            "col": data_color,
+            "ls": data_line_style,
+        }
+
+    @classmethod
+    def on_exonic_only(
+        cls,
+        null_hypothesis_samples: pd.DataFrame,
+        data_results: pd.DataFrame,
+        column_key: str,
+        sample_label: str,
+        sample_color: str,
+        sample_alpha: float,
+        sample_hatch: str | None,
+        sample_hist_type: str,
+        data_label: str,
+        data_color: str,
+        data_line_style: str,
+    ):
+        pass
+
+    @classmethod
+    def off_exonic_only(
+        cls,
+        null_hypothesis_samples: pd.DataFrame,
+        data_results: pd.DataFrame,
+        column_key: str,
+        sample_label: str,
+        sample_color: str,
+        sample_alpha: float,
+        sample_hatch: str | None,
+        sample_hist_type: str,
+        data_label: str,
+        data_color: str,
+        data_line_style: str,
+    ):
+        pass
+
+    @classmethod
+    def on_exonic_intronic(
+        cls,
+        null_hypothesis_samples: pd.DataFrame,
+        data_results: pd.DataFrame,
+        column_key: str,
+        sample_label: str,
+        sample_color: str,
+        sample_alpha: float,
+        sample_hatch: str | None,
+        sample_hist_type: str,
+        data_label: str,
+        data_color: str,
+        data_line_style: str,
+    ):
+        pass
+
+    @classmethod
+    def off_exonic_intronic(
+        cls,
+        null_hypothesis_samples: pd.DataFrame,
+        data_results: pd.DataFrame,
+        column_key: str,
+        sample_label: str,
+        sample_color: str,
+        sample_alpha: float,
+        sample_hatch: str | None,
+        sample_hist_type: str,
+        data_label: str,
+        data_color: str,
+        data_line_style: str,
+    ):
+        pass
